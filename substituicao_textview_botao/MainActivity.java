498acdb1ecde8b3096a3abc2b1ac470decc2a15b
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-04-17 06:50:10 -0300

    MainActivity.java

---
+++ substituicao_textview_botao/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.git_botao_texto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    private EditText novoTexto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void substituirTexto(View view){
+        texto       = findViewById(R.id.textViewTexto);
+        novoTexto   = findViewById(R.id.editTextNovoTexto);
+
+        texto.setText(novoTexto.getText().toString());
+        novoTexto.setText("");
+    }
+}

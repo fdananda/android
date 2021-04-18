60eb5bb3d9cea66fb0996e937b86c5745f9a4ec7
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-04-18 07:02:40 -0300

    MainActivity.java

---
+++ sorteio_string[]/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gitsorteiodelista;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    String[] opcoes = {"Opção 1", "Opção 2", "Opção 3", "Opção 4", "Opção 5",
+            "Opção 6", "Opção 7", "Opção 8", "Opção 9", "Opção 10"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view){
+        texto               = findViewById(R.id.textViewTexto);
+        int numeroSorteado  = new Random().nextInt(10);
+        texto.setText(opcoes[numeroSorteado]);
+    }
+}

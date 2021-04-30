e92af5d48a6c3389d545813e1c1e79795100ec97
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-04-30 10:26:37 -0300

    PrimeiroFragment.java

---
+++ navigation_drawer/PrimeiroFragment.java
@@ -0,0 +1,21 @@
+package com.fdananda.gitnavigationdrawer;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PrimeiroFragment extends Fragment {
+
+    public PrimeiroFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primeiro, container, false);
+    }
+}

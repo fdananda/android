9b987560655022cf61e2d29e2a99e410c80105c1
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-05-04 18:43:06 -0300

    Atributo.java

---
+++ crud_sharedpreferences_v2/Atributo.java
@@ -0,0 +1,35 @@
+package com.fdananda.gitcrudsharedpreferences;
+
+public class Atributo {
+
+    private String atributo1;
+    private String atributo2;
+    private String atributo3;
+
+    public Atributo() {
+    }
+
+    public String getAtributo1() {
+        return atributo1;
+    }
+
+    public void setAtributo1(String atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    public String getAtributo2() {
+        return atributo2;
+    }
+
+    public void setAtributo2(String atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    public String getAtributo3() {
+        return atributo3;
+    }
+
+    public void setAtributo3(String atributo3) {
+        this.atributo3 = atributo3;
+    }
+}

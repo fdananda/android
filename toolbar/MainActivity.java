be830e7a561af46a7c6572386c75a6bb9035a4de
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-05-18 07:51:49 -0300

    MainActivity.java

---
+++ toolbar/MainActivity.java
@@ -0,0 +1,61 @@
+package com.fdananda.gittoolbar;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewOpcaoSelecionada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Git Toolbar");
+        toolbar.setSubtitle("Subtítulo Toolbar Personalizada");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setSubtitleTextColor(getResources().getColor(R.color.design_default_color_background));
+        toolbar.setTitleMarginBottom(5);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        textViewOpcaoSelecionada  = findViewById(R.id.textViewOpcaoSelecionada);
+
+        switch (item.getItemId()){
+            case R.id.toast1:
+                textViewOpcaoSelecionada.setText("Selecionada a opção Toast 1");
+                Toast.makeText(this, "Selecionada a opção Toast 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.toast2:
+                textViewOpcaoSelecionada.setText("Selecionada a opção Toast 2");
+                Toast.makeText(this, "Selecionada a opção Toast 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.toast3:
+                textViewOpcaoSelecionada.setText("Selecionada a opção Toast 3");
+                Toast.makeText(this, "Selecionada a opção Toast 3", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sair:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

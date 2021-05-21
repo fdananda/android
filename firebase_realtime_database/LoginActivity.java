69d1a0682b6cb347542faced9a91ece13fb7cba0
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-05-21 18:35:55 -0300

    LoginActivity.java

---
+++ firebase_realtime_database/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.fdananda.gitfirebaserealtimedatabase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmailLogar, editTextSenhaLogar;
+    private TextView textViewMensagemErroLogar;
+    String mensagemErro = "";
+    private FirebaseAuth autenticacao = Configuracao.getAutenticacao();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextEmailLogar = findViewById(R.id.editTextEmailLogar);
+        editTextSenhaLogar = findViewById(R.id.editTextSenhaLogar);
+        textViewMensagemErroLogar = findViewById(R.id.textViewMensagemErroLogar);
+    }
+
+    public void logarUsuario(View view){
+
+        String email = editTextEmailLogar.getText().toString();
+        String senha = editTextSenhaLogar.getText().toString();
+
+        if (autenticacao.getCurrentUser() != null) {
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("email", autenticacao.getCurrentUser().getEmail());
+            intent.putExtra("uid", autenticacao.getCurrentUser().getUid());
+            startActivity(intent);
+        }else{
+
+        if (email.isEmpty() || email == "" || !email.contains("@")) {
+
+            mensagemErro = "Preencha um e-mail válido!";
+            textViewMensagemErroLogar.setText(mensagemErro);
+        } else {
+            if (senha.isEmpty() || senha == "" || senha.length() < 6) {
+
+                mensagemErro = "Preencha uma senha válida!";
+                textViewMensagemErroLogar.setText(mensagemErro);
+            } else {
+
+                Usuario usuario = new Usuario();
+                usuario.setEmail(email);
+                usuario.setSenha(senha);
+
+                //Logar usuário
+                autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    mensagemErro = "Usuário logado com sucesso: " + task.getResult().getUser().getEmail();
+                                    textViewMensagemErroLogar.setText(mensagemErro);
+                                    textViewMensagemErroLogar.setTextColor(getResources().getColor(R.color.black));
+
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    intent.putExtra("email", task.getResult().getUser().getEmail());
+                                    intent.putExtra("uid", task.getResult().getUser().getUid());
+                                    startActivity(intent);
+
+                                } else {
+
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthInvalidCredentialsException exception) {
+                                        mensagemErro = "Erro ao logar: E-mail inválido!";
+                                    } catch (FirebaseAuthInvalidUserException exception) {
+                                        mensagemErro = "Erro ao logar: Usuário não localizado!";
+                                    } catch (Exception exception) {
+                                        mensagemErro = "Erro ao logar: " + exception.getMessage();
+                                    }
+                                    textViewMensagemErroLogar.setText(mensagemErro);
+                                }
+                            }
+                        });
+            }
+            }
+        }
+    }
+
+}

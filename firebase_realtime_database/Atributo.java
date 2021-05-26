d1873b235b5b04890ebb5cd91a083587e62c512d
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-05-26 08:56:26 -0300

    Update Atributo.java

--- firebase_realtime_database/Atributo.java
+++ firebase_realtime_database/Atributo.java
@@ -1,5 +1,10 @@
 package com.fdananda.gitfirebaserealtimedatabase;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Atributo {
 
     private String atributo1;
@@ -8,6 +13,37 @@ public class Atributo {
     private String identificador;
 
     public Atributo() {
+    }
+
+    public void salvar(){
+
+        DatabaseReference atributosRef = Configuracao.getDatabase();
+        //Ao usar o push() no lugar do child(), é gerado um identificador único ao salvar
+        atributosRef.push().setValue(this);
+    }
+
+    public void editar(){
+
+        DatabaseReference atributosRefAlterar = Configuracao.getDatabase().child(this.getIdentificador());
+
+        Map<String, Object> valAtributos = toMap();
+        atributosRefAlterar.updateChildren(valAtributos);
+    }
+
+    public void excluir(){
+
+        DatabaseReference atributosRefSelecionado = Configuracao.getDatabase().child(this.getIdentificador());
+        atributosRefSelecionado.removeValue();
+    }
+
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> atributoMap = new HashMap<>();
+        atributoMap.put("atributo1", getAtributo1());
+        atributoMap.put("atributo2", getAtributo2());
+        atributoMap.put("atributo3", getAtributo3());
+
+        return atributoMap;
     }
 
     public String getIdentificador() {

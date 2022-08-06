ad6545901377e5ca3fb01193769900c9e89bb20e
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2022-08-06 01:12:15 -0300

    Create MainActivity.java

---
+++ no_title/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fdananda.darktheme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}

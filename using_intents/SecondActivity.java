95d70eb4464e64975443aa637216cb16b89a86a2
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2022-08-07 00:24:13 -0300

    Create SecondActivity.java

---
+++ using_intents/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.fdananda.usingintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}

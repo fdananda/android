930521987aa0f1f2eada04f9f19f52dcf8af0882
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2022-08-07 00:23:38 -0300

    Create MainActivity.java

---
+++ using_intents/MainActivity.java
@@ -0,0 +1,19 @@
+package com.fdananda.usingintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        startActivity(new Intent("com.fdananda.usingintent.SecondActivity"));
+    }
+}
